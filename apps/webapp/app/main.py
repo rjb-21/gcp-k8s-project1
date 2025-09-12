e830926e7a4ae7fd90eb23ffd32a935e695e02b1
rjb-21 <radek0101@gmail.com>
2025-09-12 12:46:35 +0200

    fix: update Docker image tag format and remove hardcoded tag from Helm parameters

--- apps/webapp/app/main.py
+++ apps/webapp/app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from GKE with ArgoCD and Helm and CI/CD!!"
+    return "Hello from GKE with ArgoCD and Helm and CI/CD!!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
